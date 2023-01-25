b47742d2ffdba463c9b912c388d1ef3e782538ac
luisaguimaraes <luisavitoriaserro@hotmail.com>
2023-01-25 15:46:00 -0300

    Detalhes de tarefa

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -5,20 +5,29 @@ namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller
 {
+    public List<TarefaViewModel> listaDeTarefas { get; set;}
+    public TarefasController()
+    {
+        listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel(){ Id = 1, Titulo = "Escovar os dentes" , Descricao = "Lembrar de usar o Listerine" },
+            new TarefaViewModel(){ Id = 2, Titulo = "Arrumar a cama" , Descricao = "Alinhar o lençol de elástico"},
+            new TarefaViewModel(){ Id = 3, Titulo = "Por o lixo pra fora" , Descricao = "Somente às terças, quintas e sábados."}
+        };
+    }
     public IActionResult Create()
     {
         return View();
     }
 
     public IActionResult Index()
-    {
-        var listaDeTarefas = new List<TarefaViewModel>()
-        {
-            new TarefaViewModel(){ Id = 1, Titulo = "Escovar os dentes" , Descricao = "Lembrar de usar o Listerine" },
-            new TarefaViewModel(){ Id = 2, Titulo = "Arrumar a cama" , Descricao = "Alinhar o lençol de elástico"},
-            new TarefaViewModel(){ Id = 3, Titulo = "Por o lixo pra fora" , Descricao = "Somente às terças, quintas e sábados."}
-        };
-
+    {      
         return View(listaDeTarefas);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+        return View(tarefa);
     }
 }
