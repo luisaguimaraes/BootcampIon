9e7ca6ce912daa152926b6d40fbd68d2e1a840d7
luisaguimaraes <kaiquex71@gmail.com>
2023-01-25 13:55:54 -0300

    Lista de tarefas

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
@@ -7,5 +8,17 @@ public class TarefasController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+        var listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel(){ Titulo = "Escovar os dentes" , Descricao = "Lembrar de usar o Listerine" },
+            new TarefaViewModel(){ Titulo = "Arrumar a cama" , Descricao = "Alinhar o lençol de elástico"},
+            new TarefaViewModel(){ Titulo = "Por o lixo pra fora" , Descricao = "Somente às terças, quintas e sábados."}
+        };
+
+        return View(listaDeTarefas);
     }
 }
